caeef894c1ff386f82c5436601d8a7444451c014
Jan Graefen <jan.graefen@rwth-aachen.de>
2011-07-24 17:00:26 +0200

    VariableAstNode added and is now considered by the parser

---
+++ Build/Generation/Ast/VariableAstNode.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Noize\Build\Generation\Ast;
+
+/**
+ * A variable node of the PHP abstract syntax tree.
+ *
+ * @final
+ * @author Jan Gräfen
+ * @package Build
+ * @subpackage Generation\Ast
+ * @version 1.0
+ */
+final class VariableAstNode extends BaseAstNode {
+
+    /**
+     * Stores the default value of the current variable node.
+     *
+     * @var string
+     */
+    private $defaultValue = null;
+
+    /**
+     * Returns TRUE if the current node has a default value, FALSE otherwise.
+     *
+     * @return bool
+     */
+    public function hasDefaultValue() {
+        return ($this->defaultValue != null);
+    }
+
+    /**
+     * Returns the default value of the current variable node.
+     *
+     * @return string
+     */
+    public function getDefaultValue() {
+        return $this->defaultValue;
+    }
+
+    /**
+     * Sets a new default value for the current variable node.
+     *
+     * @param string $defaultValue A new default value.
+     */
+    public function setDefaultValue($defaultValue) {
+        $this->defaultValue = $defaultValue;
+    }
+
+    /**
+     * This function generates PHP code from this node and returns it.
+     * 
+     * @return string
+     */
+    public function generate() {
+        $attrString = '';
+
+        foreach ($this->attributes as $attr)
+            $attrString .= $attr->generate() . ' ';
+
+        $code = $attrString . $this->text;
+
+        if ($this->hasDefaultValue())
+            $code .= ' = ' . $this->defaultValue;
+
+        return $code;
+    }
+}
+
+?>
